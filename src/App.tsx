e093901865373e090bb7003497084d9691b7a520
Prashant Kamboj <prashant.kamboj4@gmail.com>
2025-05-16 00:33:38 +0530

    fix btn

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <div>main changes</div>
       <div>second change</div>
       <Button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        increment
+        increment btn
       </Button>
       {count}
       <div>Next change</div>
